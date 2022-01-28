43246e8293d448d1554cbc35de91b179262e34a8
phobo3s <bberkkilinc@gmail.com>
2022-01-28 18:37:57 +0300

    Halfway there

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.logging.Logger;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        // Numbers file path
         String numbersPath = "C:\\Java_Code\\Sorter\\src\\NumbersFolder\\NumbersFile.txt";
         File numbersFile = new File(numbersPath);
+        // Take numbers to an ArrayList
         ArrayList<Integer> numbersList = numberFileReader(numbersFile);
-        Logger.getLogger("sd");
+        ArrayList<Integer> sortedNumberArray = new ArrayList<Integer>();
+        sortedNumberArray = radixSort(numbersList, sortedNumberArray);
+
     }
 
     public static ArrayList<Integer> numberFileReader(File numbersFile) {
@@ -33,8 +33,23 @@ public class Main {
         return numbersList;
     }
 
-    public static void lineToArrayList(String line, ArrayList numbersArray) {
-
+    public static ArrayList<Integer> radixSort(ArrayList<Integer> numberArray, ArrayList<Integer> sortedNumberArray) {
+        int[] digitArray = new int[10];
+        // Radix sort implementation
+        for (int i = 0; i <= 3; i++) {
+            for (int number : numberArray) {
+                try {
+                    digitArray[Integer.parseInt(String.valueOf(number).substring(
+                            String.valueOf(number).length() - i - 1, String.valueOf(number).length() - i))]++;
+                } catch (StringIndexOutOfBoundsException e) {
+                    // e.getStackTrace();
+                } catch (Exception e) {
+                    // e.getStackTrace();
+                }
+            }
+            sortedNumberArray.add(1);
+        }
+        return sortedNumberArray;
     }
 
 }
