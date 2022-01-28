d0df7bbee1f44bc4929b61d64a2e017a403ad5e5
phobo3s <bberkkilinc@gmail.com>
2022-01-28 13:25:34 +0300

    FileReading

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        String numbersPath = "C:\\Java_Code\\Sorter\\src\\NumbersFolder\\NumbersFile.txt";
+        File numbersFile = new File(numbersPath);
+        ArrayList<Integer> numbersList = numberFileReader(numbersFile);
+        Logger.getLogger("sd");
     }
+
+    public static ArrayList<Integer> numberFileReader(File numbersFile) {
+        ArrayList<Integer> numbersList = new ArrayList<Integer>();
+        try {
+            Scanner reader = new Scanner(numbersFile);
+            while (reader.hasNextLine()) {
+                numbersList.add(Integer.parseInt(reader.nextLine()));
+            }
+            reader.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+
+        }
+        return numbersList;
+    }
+
+    public static void lineToArrayList(String line, ArrayList numbersArray) {
+
+    }
+
 }
