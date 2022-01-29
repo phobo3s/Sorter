614bf8689a1a1811039b8aaa98e477b7ec00129a
phobo3s <bberkkilinc@gmail.com>
2022-01-29 18:00:54 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,15 @@ public class Main {
         // Numbers file path
         String numbersPath = "C:\\Java_Code\\Sorter\\src\\NumbersFolder\\NumbersFile.txt";
         arrayToTerminal(new RadixSorter(numbersPath, 4).getResult());
+
     }
 
     public static void arrayToTerminal(ArrayList<Integer> sortedNumberArray) {
         System.out.println("Radix Sıralama Tamamlandı.");
-        System.out.println("Sonuçlar :");
+        System.out.print("Sonuçlar : {");
         for (int number : sortedNumberArray) {
-            System.out.println(number);
+            System.out.print(number + ",");
         }
+        System.out.println("}");
     }
 }
