8a3df5d0ca4e96a68c8f87e85fb67de14d007e08
phobo3s <bberkkilinc@gmail.com>
2022-01-29 17:48:00 +0300

    it is working and made a class for Radix seperately

--- src/Main.java
+++ src/Main.java
@@ -1,99 +1,18 @@
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // Numbers file path
         String numbersPath = "C:\\Java_Code\\Sorter\\src\\NumbersFolder\\NumbersFile.txt";
-        File numbersFile = new File(numbersPath);
-        // Take numbers to an ArrayList
-        ArrayList<Integer> numbersList = numberFileReader(numbersFile);
-        ArrayList<Integer> sortedNumberArray = radixSort(numbersList, 3);
-        sortedNumbersToTerminal(sortedNumberArray);
+        arrayToTerminal(new RadixSorter(numbersPath, 4).getResult());
     }
 
-    public static void sortedNumbersToTerminal(ArrayList<Integer> sortedNumberArray) {
+    public static void arrayToTerminal(ArrayList<Integer> sortedNumberArray) {
         System.out.println("Radix Sıralama Tamamlandı.");
         System.out.println("Sonuçlar :");
         for (int number : sortedNumberArray) {
             System.out.println(number);
         }
     }
-
-    public static ArrayList<ArrayList<Integer>> radixBucketsCreator() {
-        // 10 arraylist for 10 digits inside an array list.
-        ArrayList<ArrayList<Integer>> radixBuckets = new ArrayList<ArrayList<Integer>>();
-        for (int i = 0; i <= 9; i++) {
-            radixBuckets.add(new ArrayList<Integer>());
-        }
-        return radixBuckets;
-    }
-
-    public static ArrayList<Integer> numberFileReader(File numbersFile) {
-        ArrayList<Integer> numbersList = new ArrayList<Integer>();
-        try {
-            Scanner reader = new Scanner(numbersFile);
-            while (reader.hasNextLine()) {
-                numbersList.add(Integer.parseInt(reader.nextLine()));
-            }
-            reader.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } finally {
-
-        }
-        return numbersList;
-    }
-
-    public static ArrayList<Integer> radixSort(ArrayList<Integer> numberArray, int highestDigitCount) {
-        // Digit counts start from 0 so minus 1.
-        highestDigitCount = highestDigitCount - 1;
-        // get radix buckets.
-        ArrayList<ArrayList<Integer>> radixBuckets = radixBucketsCreator();
-        // for every digit.
-        for (int i = 0; i <= highestDigitCount; i++) {
-            for (int number : numberArray) {
-                // find i'nth digit
-                int digit;
-                try {
-                    digit = findNthDigit(number, i);
-                    radixBuckets.get(digit).add(number);
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    // e.printStackTrace();
-                }
-            }
-            // From buckets to numberArray
-            numberArray.clear();
-            for (ArrayList<Integer> bucket : radixBuckets) {
-                if (bucket.size() != 0) {
-                    for (int num : bucket) {
-                        numberArray.add(num);
-                    }
-                }
-                bucket.clear();
-            }
-            System.out.println("Soldan " + i + ". basamak bitti.");
-        }
-        return numberArray;
-    }
-
-    public static Integer findNthDigit(Integer number, Integer n) throws Exception {
-        // n is from last digit. Starts with 0.
-        int digit;
-        // now it starts with 1. Sorry.
-        n = n + 1;
-        if (n > number.toString().length()) {
-            digit = 0;
-            // throw new Exception("OutOfIntegerDigitsError");
-        }
-        digit = (number % (int) Math.pow(10, n)) - (number % (int) Math.pow(10, (n - 1)));
-        digit = digit / (int) Math.pow(10, (n - 1));
-        return digit;
-    }
-
 }
---
+++ src/RadixSorter.java
@@ -0,0 +1,94 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RadixSorter {
+
+    private File numbersFile;
+    private ArrayList<Integer> resultArray;
+
+    public RadixSorter(String numbersPath, int maxDigitCount) {
+        numbersFile = new File(numbersPath);
+        resultArray = new ArrayList<Integer>();
+        this.resultArray = radixSort(numberFileReader(numbersFile), maxDigitCount);
+    }
+
+    public void main() {
+
+    }
+
+    public ArrayList<Integer> getResult() {
+        return this.resultArray;
+    }
+
+    public ArrayList<ArrayList<Integer>> radixBucketsCreator() {
+        // 10 arraylist for 10 digits inside an array list.
+        ArrayList<ArrayList<Integer>> radixBuckets = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i <= 9; i++) {
+            radixBuckets.add(new ArrayList<Integer>());
+        }
+        return radixBuckets;
+    }
+
+    public ArrayList<Integer> numberFileReader(File numbersFile) {
+        ArrayList<Integer> numbersList = new ArrayList<Integer>();
+        try {
+            Scanner reader = new Scanner(numbersFile);
+            while (reader.hasNextLine()) {
+                numbersList.add(Integer.parseInt(reader.nextLine()));
+            }
+            reader.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } finally {
+
+        }
+        return numbersList;
+    }
+
+    public ArrayList<Integer> radixSort(ArrayList<Integer> numberArray, int highestDigitCount) {
+        // Digit counts start from 0 so minus 1.
+        highestDigitCount = highestDigitCount - 1;
+        // get radix buckets.
+        ArrayList<ArrayList<Integer>> radixBuckets = radixBucketsCreator();
+        // for every digit.
+        for (int i = 0; i <= highestDigitCount; i++) {
+            for (int number : numberArray) {
+                // find i'nth digit
+                int digit;
+                digit = findNthDigit(number, i);
+                radixBuckets.get(digit).add(number);
+            }
+            // From buckets to numberArray
+            numberArray.clear();
+            // sor is starting from 0 so itis ascending order.
+            for (ArrayList<Integer> bucket : radixBuckets) {
+                if (bucket.size() != 0) {
+                    for (int num : bucket) {
+                        numberArray.add(num);
+                    }
+                }
+                bucket.clear();
+            }
+            System.out.println("Soldan " + i + ". basamak bitti.");
+        }
+        return numberArray;
+    }
+
+    public Integer findNthDigit(Integer number, Integer n) {
+        // n is from last digit. Starts with 0.
+        int digit;
+        // now it starts with 1. Sorry.
+        n = n + 1;
+        if (n > number.toString().length()) {
+            digit = 0;
+            // throw new Exception("OutOfIntegerDigitsError");
+        }
+        digit = (number % (int) Math.pow(10, n)) - (number % (int) Math.pow(10, (n - 1)));
+        digit = digit / (int) Math.pow(10, (n - 1));
+        return digit;
+    }
+
+}
