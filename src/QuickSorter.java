8cdaea69290909a03a2e8365630736e73182c1b9
phobo3s <bberkkilinc@gmail.com>
2022-02-01 09:23:01 +0300

    ok.

--- src/QuickSorter.java
+++ src/QuickSorter.java
@@ -1,3 +1,16 @@
+import java.io.File;
+import java.util.ArrayList;
+
 public class QuickSorter {
+
+    private ArrayList<Integer> resultArray;
+
+    public QuickSorter(String numbersPath) {
+        File numbersFile = new File(numbersPath);
+        ArrayList<Integer> numberArrayList = new ArrayList<Integer>(
+                HelperFunctions.NumberFileToListReader(numbersFile));
+        // this.resultArray = radixSort(numberArrayList,
+        // findMaxDigitCount(numberArrayList));
+    }
 
 }
